d20c3a960ee6d1eda866818a78f3a861bee589fe
ridhamlangnecha <ridhamlangnecha62852@gmail.com>
2024-12-30 10:01:30 +0530

    he

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('send_message', (data) => {
+        io.emit('receive_message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
